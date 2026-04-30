b1320adb7f4cce2ee57b80d75394b196420331fb
Md-Ramjan-Ali <gmramjanali8888@gmail.com>
2026-04-30 21:23:53 +0600

    initialize server configuration and base run function in service.ts

--- src/service.ts
+++ src/service.ts
@@ -2,3 +2,12 @@ import dotenv from "dotenv";
 dotenv.config();
 
 const port = process.env.PORT || 8000;
+
+
+const runServer = () => {
+    try {
+        
+    } catch (error) {
+        
+    }
+}
